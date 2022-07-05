cabb7919bedd7e1ac14ce243f41016558fa2a54b
ZSJ <zsj@qq.com>
2022-07-05 14:21:46 +0800

    second commit

--- src/main/java/com/zsj/gittest.java
+++ src/main/java/com/zsj/gittest.java
@@ -3,5 +3,6 @@ package com.zsj;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git1!");
     }
 }
